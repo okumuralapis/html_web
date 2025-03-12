cb522ab9b85ac472026b605c46c57f06fd0ae315
okumuralapis <okumuralapis.makarenko@gmail.com>
2025-03-12 16:20:51 +0400

    Цвет каюты

--- 1.py
+++ 1.py
@@ -9,11 +9,9 @@ def index(title):
     return render_template('base.html', title=title)
 
 
-@app.route('/distribution')
-def distr():
-    return render_template('distribution.html', names=['Ридли Скотт', 'Энди Уир', 'Марк Уортни',
-                                                      'Венката Капур', 'Тедди Санднерс', 'Шон Бин'])
-
+@app.route('/table/<sex>/<age>')
+def distr(sex, age):
+    return render_template('desic.html',sex=sex, age=int(age))
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')

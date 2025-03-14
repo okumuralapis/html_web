5f312d85c94b3d2354212c4ba893d06ce5da2e76
okumuralapis <okumuralapis.makarenko@gmail.com>
2025-03-14 22:30:37 +0400

    Загрузка файла

--- 1.py
+++ 1.py
@@ -1,20 +1,42 @@
-from flask import Flask, render_template
+# 1 доп html
+from flask import Flask, request, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/<title>')
-@app.route('/index/<title>')
-def index(title):
-    return render_template('base.html', title=title)
+@app.route('/sample_file_upload', methods=['GET', 'PUT'])
+def sample_file_upload():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                             <link rel="stylesheet"
+                             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                             crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Пример загрузки файла</title>
+                          </head>
+                          <body>
+                            <h1>Загрузим файл</h1>
+                            <form method="get" enctype="multipart/form-data">
+                                <div class="form-group">
+                                    <label for="photo">Выберите файл</label>
+                                    <input type="file" class="form-control-file" id="photo" name="file">
+                                    <img src=file>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Отправить</button>
+                            </form>
+                          </body>
+                        </html>'''
+    elif request.method == 'PUT':
+        print('1')
+        f = request.files['file']
+        print(f.read())
+        return "Форма отправлена"
 
-
-@app.route('/table/<sex>/<age>')
-def distr(sex, age):
-    return render_template('desic.html',sex=sex, age=int(age))
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
-
-
-print(1)

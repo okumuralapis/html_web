17a91f057cf134469edc60cb9c50c2b432d27271
okumuralapis <okumuralapis.makarenko@gmail.com>
2025-03-15 15:39:31 +0400

    Первая работа

---
+++ data/jobs.py
@@ -0,0 +1,20 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+class Job(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer,
+                                    sqlalchemy.ForeignKey('users.id'))
+    job = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    start_date = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    end_date = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean, nullable=False)
+    user = orm.relationship('User')

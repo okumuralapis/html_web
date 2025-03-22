f4ef0509a90b1b85f4a984af65881fa0545f6b05
okumuralapis <okumuralapis.makarenko@gmail.com>
2025-03-22 18:20:48 +0400

    Форма регистрации

--- sqlachemy_1.py
+++ sqlachemy_1.py
@@ -1,8 +1,9 @@
 from data import db_session
 from data.users import User
+from forms.user import RegisterForm
 from data.jobs import Job
 import sqlalchemy
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -13,11 +14,32 @@ def main():
     app.run()
 
 
-@app.route("/")
-def index():
-    db_sess = db_session.create_session()
-    info = db_sess.query(Job, User).join(User).all()
-    return render_template('index.html', info=info)
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data,
+            email=form.email.data)
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':

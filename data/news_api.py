77a498838537999ec651a31ae3b15a68175e16c3
okumuralapis <okumuralapis.makarenko@gmail.com>
2025-04-09 18:20:54 +0300

    api hihihi primer

--- data/news_api.py
+++ data/news_api.py
@@ -1,5 +1,5 @@
 import flask
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 from . import db_session
 from .news import News
 
@@ -9,6 +9,7 @@ blueprint = flask.Blueprint(
     template_folder='templates'
 )
 app = flask.Flask(__name__)
+
 
 @blueprint.route('/api/news')
 def get_news():
@@ -45,3 +46,33 @@ def not_found(error):
 @app.errorhandler(400)
 def bad_request(_):
     return make_response(jsonify({'error': 'Bad Request'}), 400)
+
+
+@blueprint.route('/api/news', methods=['POST'])
+def create_news():
+    if not request.json:
+        return make_response(jsonify({'error': 'Empty request'}), 400)
+    elif not all(key in request.json for key in
+                 ['title', 'content', 'user_id', 'is_private']):
+        return make_response(jsonify({'error': 'Bad request'}), 400)
+    db_sess = db_session.create_session()
+    news = News(
+        title=request.json['title'],
+        content=request.json['content'],
+        user_id=request.json['user_id'],
+        is_private=request.json['is_private']
+    )
+    db_sess.add(news)
+    db_sess.commit()
+    return jsonify({'id': news.id})
+
+
+@blueprint.route('/api/news/<int:news_id>', methods=['DELETE'])
+def delete_news(news_id):
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).get(news_id)
+    if not news:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    db_sess.delete(news)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})

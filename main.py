a638b015f8dbb74717a84053e093e4276389b8f9
okumuralapis <okumuralapis.makarenko@gmail.com>
2025-04-11 15:40:29 +0300

    Использование API

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, redirect, request, abort
-from data import db_session, news_api
+from data import db_session, news_api, news_resources
 from data.users import User
 from data.news import News
 from forms.user import RegisterForm, LoginForm
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from forms.news import NewsForm
-
+from flask_restful import reqparse, abort, Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 login_manager = LoginManager()
 login_manager.init_app(app)
+# для списка объектов
+api.add_resource(news_resources.NewsListResource, '/api/v2/news')
+
+# для одного объекта
+api.add_resource(news_resources.NewsResource, '/api/v2/news/<int:news_id>')
 
 
 @login_manager.user_loader
